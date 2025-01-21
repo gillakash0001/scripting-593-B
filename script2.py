18e02a2be7fed3126234b2e4a9735388832da7c7
gillakash0001 <Asingh376@flemingcollege.ca>
2025-01-21 00:35:07 -0500

    Update script2.py

--- script2.py
+++ script2.py
@@ -1,4 +1,8 @@
 # this is good programming advice 
+<<<<<<< Updated upstream
 print("if the implementation is hard to explain, it's a bad idea.")
 print("if the implementation is easy to explain, it may be a good idea.")
 print("Namespaces are one honking great idea -- let's do more of those!")
+=======
+print("if the implementation is hard to explain, it's a bad idea.")-+
+>>>>>>> Stashed changes
